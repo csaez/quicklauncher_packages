b655d3c71dfdb9c9910bbdbf8a3fb8545e684409
csaez <cesarte@gmail.com>
2014-01-07 02:26:13 -0300

    A quick foreach loop on selection

---
+++ programming/batch_selection.py
@@ -0,0 +1,52 @@
+from PyQt4 import QtCore, QtGui
+from wishlib.qt.QtGui import QDialog
+from wishlib.si import *
+
+
+class BatchSelection(QDialog):
+
+    def __init__(self, parent=None):
+        super(BatchSelection, self).__init__(parent)
+        self.setup_ui()
+        self.execute_button.clicked.connect(self.execute_clicked)
+        # self.code_plainTextEdit.setPlainText("obj.Name = ")
+
+    def setup_ui(self):
+        self.resize(294, 281)
+        self.verticalLayout = QtGui.QVBoxLayout(self)
+        self.verticalLayout.setSpacing(3)
+        self.verticalLayout.setMargin(6)
+        self.forloop_label = QtGui.QLabel(self)
+        self.forloop_label.setText(
+            "from wishlib.si import *\nfor i, obj in enumerate(sisel):")
+        self.verticalLayout.addWidget(self.forloop_label)
+        self.horizontalLayout = QtGui.QHBoxLayout()
+        self.indent_label = QtGui.QLabel(self)
+        self.indent_label.setText("    ")
+        self.horizontalLayout.addWidget(self.indent_label)
+        self.code_plainTextEdit = QtGui.QPlainTextEdit(self)
+        self.code_plainTextEdit.setTabStopWidth(12)
+        self.horizontalLayout.addWidget(self.code_plainTextEdit)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+        self.execute_button = QtGui.QToolButton(self)
+        sizePolicy = QtGui.QSizePolicy(
+            QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(
+            self.execute_button.sizePolicy().hasHeightForWidth())
+        self.execute_button.setSizePolicy(sizePolicy)
+        self.execute_button.setMinimumSize(QtCore.QSize(0, 25))
+        self.execute_button.setText("EXECUTE")
+        self.verticalLayout.addWidget(self.execute_button)
+        self.indent_label.setBuddy(self.code_plainTextEdit)
+        self.setTabOrder(self.code_plainTextEdit, self.execute_button)
+
+    def execute_clicked(self):
+        code = "from wishlib.si import *\n\n"
+        code += "for i, obj in enumerate(sisel):\n    "
+        code += str(self.code_plainTextEdit.toPlainText()).replace(
+            "\n", "\n    ")
+        exec(code)
+
+show_qt(BatchSelection)
